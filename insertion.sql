49783d0da9de20e2bae535201e20616692c0299b
thompaw <98345171+thompaw@users.noreply.github.com>
2022-04-08 13:26:01 -0400

    fixed insertion script

--- insertion.sql
+++ insertion.sql
@@ -6,20 +6,20 @@ VALUES
 (4000, 200, 3800, "premium", 35, "Free Bathrooms"), 
 (12038, 12, 12026, "regular", 103, NULL);
 
-INSERT INTO Winnings(Patron_id, amount_won, [table], game, game_id)
+INSERT INTO Game(Number_of_players, table_location, game_desc)
+VALUES
+(14, 'AA', 'Blackjack'), 
+(5, 'AB', 'Poker'), 
+(15, 'AC', 'GoFish');
+
+INSERT INTO Winnings(Patron_id, amount_won, table_location, game, game_id)
 VALUES
 (1, 100, 'AC', 'GoFish', 3), 
 (2, 4000, 'AA', 'Blackjack', 1), 
 (3, 12038, 'AB', 'Poker', 2);
 
-INSERT INTO Losses(Patron_id, amount_lost, [table], game, game_id)
+INSERT INTO Losses(Patron_id, amount_lost, table_location, game, game_id)
 VALUES
 (2, 200, 'AB', 'Poker', 2), 
 (3, 12, 'AA', 'Blackjack', 1), 
 (1, 1000, 'AC', 'GoFish', 3);
-
-INSERT INTO Game(Number_of_players, [table], [description])
-VALUES
-(14, 'AA', 'Blackjack'), 
-(5, 'AB', 'Poker'), 
-(15 'AC', 'GoFish');
