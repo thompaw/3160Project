0af4e1db718d86576d24c90e852c8deabfb8050f
Thomas Pawlowski <98345171+thompaw@users.noreply.github.com>
2022-04-07 20:41:40 -0400

    restructured schema,make insertion file

--- schema.sql
+++ schema.sql
@@ -2,35 +2,45 @@ DROP DATABASE IF EXISTS Casino;
 CREATE DATABASE Casino;
 USE Casino; 
 
-CREATE TABLE Patrons (
-    Account_type varchar(15) NOT NULL
-);
-
-CREATE TABLE Account (
+CREATE TABLE Patron (
     account_id int NOT NULL AUTO_INCREMENT, 
-    [name] varchar(40) NOT NULL, 
-    last4 int NOT NULL, 
-    phone_number int NOT NULL, 
-    total_balance int NOT NULL, 
-    PRIMARY KEY (account_id)
+    Winnings int, 
+    Losses int, 
+    Net_Profit int NOT NULL,
+    Patron_type varchar(10) NOT NULL, 
+    Games_Played int, 
+    Perks varchar(255), 
+    PRIMARY KEY (account_id) 
 );
 
 CREATE TABLE Winnings (
-    account_id int NOT NULL, 
+    Patron_id int NOT NULL, 
     transaction_id int NOT NULL AUTO_INCREMENT, 
     amount_won int NOT NULL, 
     [table] varchar(2) NOT NULL, 
-    game varchar(20) NOT NULL, 
+    game varchar(15) NOT NULL, 
+    game_id int NOT NULL, 
     PRIMARY KEY (transaction_id), 
-    FOREIGN KEY (account_id) REFERENCES Account(account_id)
+    FOREIGN KEY (Patron_id) REFERENCES Patron(Patron_id),
+    FOREIGN KEY (game_id) REFERENCES Game(game_id)
 );
 
 CREATE TABLE Losses (
-    account_id int NOT NULL, 
+    Patron_id int NOT NULL, 
     transaction_id int NOT NULL AUTO_INCREMENT, 
     amount_lost int NOT NULL, 
     [table] varchar(2) NOT NULL, 
-    game varchar(20) NOT NULL, 
+    game varchar(15) NOT NULL, 
+    game_id int NOT NULL, 
     PRIMARY KEY (transaction_id), 
-    FOREIGN KEY (account_id) REFERENCES Account(account_id)
+    FOREIGN KEY (Patron_id) REFERENCES Patron(Patron_id), 
+    FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
+
+CREATE TABLE Game (
+    Game_id int NOT NULL AUTO_INCREMENT, 
+    Number_of_player int NOT NULL, 
+    [table] varchar(2) NOT NULL, 
+    [description] varchar(255), 
+    PRIMARY KEY (Game_id)
 );
