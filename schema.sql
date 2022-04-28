bed2501e1a951573cd5496715e830f2abaf35ba4
Thomas Pawlowski <98345171+thompaw@users.noreply.github.com>
2022-04-27 20:22:18 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,8 +4,6 @@ USE Casino;
 
 CREATE TABLE Patron (
     Patron_id int NOT NULL AUTO_INCREMENT, 
-    Winnings int, 
-    Losses int, 
     Net_Profit int NOT NULL,
     Patron_type varchar(10) NOT NULL, 
     Games_Played int, 
@@ -13,10 +11,17 @@ CREATE TABLE Patron (
     PRIMARY KEY (Patron_id) 
 );
 
+CREATE TABLE Employee (
+    Employee_id int NOT NULL AUTO_INCREMENT, 
+    Weekly_hours int NOT NULL,
+    Table_location varchar(2) NOT NULL, 
+    PRIMARY KEY (Employee_id),
+    FOREIGN KEY (Table_location) REFERENCES Table_(Table_location)
+);
+
 CREATE TABLE Game (
     Game_id int NOT NULL AUTO_INCREMENT, 
     Number_of_players int NOT NULL, 
-    table_location varchar(2) NOT NULL, 
     game_desc varchar(255), 
     PRIMARY KEY (Game_id)
 );
@@ -26,21 +31,24 @@ CREATE TABLE Winnings (
     Patron_id int NOT NULL, 
     amount_won int NOT NULL, 
     table_location varchar(2) NOT NULL, 
-    game varchar(15) NOT NULL, 
-    game_id int NOT NULL, 
     PRIMARY KEY (transaction_id), 
-    FOREIGN KEY (Patron_id) REFERENCES Patron(Patron_id),
-    FOREIGN KEY (game_id) REFERENCES Game(game_id)
+    FOREIGN KEY (Patron_id) REFERENCES Patron(Patron_id), 
+    FOREIGN KEY (Table_location) REFERENCES Table_(Table_location)
 );
 
 CREATE TABLE Losses (
     transaction_id int NOT NULL AUTO_INCREMENT, 
     Patron_id int NOT NULL, 
     amount_lost int NOT NULL, 
-    table_location varchar(2) NOT NULL, 
-    game varchar(15) NOT NULL, 
-    game_id int NOT NULL, 
+    table_location varchar(2) NOT NULL,
     PRIMARY KEY (transaction_id), 
     FOREIGN KEY (Patron_id) REFERENCES Patron(Patron_id), 
-    FOREIGN KEY (game_id) REFERENCES Game(game_id)
+    FOREIGN KEY (Table_location) REFERENCES Table_(Table_location)
+);
+
+CREATE TABLE Table_ (
+    Table_location varchar(2) NOT NULL, 
+    Game_id int NOT NULL, 
+    PRIMARY KEY (Table_location), 
+    FOREIGN KEY (Game_id) REFERENCES Game(Game_id)
 );
